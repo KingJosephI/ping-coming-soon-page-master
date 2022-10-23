c6552021281d64f287e6cf94135d7e7ea8469cd1
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2022-10-24 00:52:42 +0100

    feat: validate the email input before submission

---
+++ main.js
@@ -0,0 +1,22 @@
+const emailInput = document.getElementById('email');
+const btnSubmit = document.getElementById('submit');
+const errorMessage = document.getElementById('error-message');
+const validEmailPattern =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+errorMessage.style.display = 'none';
+
+emailInput.addEventListener('input', () => {
+  errorMessage.style.display = 'none';
+  emailInput.style.outline = '1px solid #C2D3FF';
+});
+
+btnSubmit.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (emailInput.value.match(validEmailPattern)) {
+    alert('Your email has beeb registered');
+  } else {
+    errorMessage.style.display = 'block';
+    emailInput.style.outline = '1px solid red';
+  }
+});
